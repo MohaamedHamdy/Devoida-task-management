4997c538bc8e9bd73acc7dd71aba380f1606c54f
MohaamedHamdy <116814421+MohaamedHamdy@users.noreply.github.com>
2025-05-08 00:10:06 +0300

    custom app bar created

---
+++ devoida_front/lib/core/widgets/custom_app_bar.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({
+    super.key,
+    required this.leading,
+    required this.title,
+    required this.actions,
+  });
+  final Widget leading;
+  final String title;
+  final List<Widget> actions;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        children: [
+          leading,
+          SizedBox(width: 10),
+          Text(title),
+          Row(children: actions),
+        ],
+      ),
+    );
+  }
+}
--- devoida_front/lib/features/home/presentation/view/home.dart
+++ devoida_front/lib/features/home/presentation/view/home.dart
@@ -1,3 +1,4 @@
+import 'package:devoida_front/core/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +6,18 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            CustomAppBar(
+              leading: Icon(Icons.favorite),
+              title: 'Workspaces',
+              actions: [],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
