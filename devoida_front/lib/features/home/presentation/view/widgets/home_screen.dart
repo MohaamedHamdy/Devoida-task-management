c5a438c000be8320650e935db2a28f8c1f4d007d
MohaamedHamdy <116814421+MohaamedHamdy@users.noreply.github.com>
2025-05-09 16:49:42 +0300

    home screen ui done

--- devoida_front/lib/features/home/presentation/view/widgets/home_screen.dart
+++ devoida_front/lib/features/home/presentation/view/widgets/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:devoida_front/core/widgets/custom_app_bar.dart';
 import 'package:devoida_front/core/widgets/custom_search_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:iconsax/iconsax.dart';
 
 class HomeScreenBody extends StatelessWidget {
   const HomeScreenBody({super.key});
@@ -35,16 +36,91 @@ class HomeScreenBody extends StatelessWidget {
           heightSizedBox(20),
           Padding(
             padding: EdgeInsets.only(left: 14.0.w, right: 14.0.w),
-            child: Column(children: [
-              CustomTextField(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomTextField(
                   prefixIcon: Icon(Icons.search),
                   hintText: "Workspaces",
                   width: double.infinity,
-                  controller: TextEditingController(),)
-            ]),
+                  controller: TextEditingController(),
+                ),
+                heightSizedBox(50),
+                Text("YOUR WORKSPACES", style: Styles.cardTitleStyle),
+                heightSizedBox(20),
+              ],
+            ),
           ),
+          WorkSpaceBlock(),
         ],
       ),
+    );
+  }
+}
+
+class WorkSpaceBlock extends StatelessWidget {
+  const WorkSpaceBlock({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(left: 14.0.w, right: 14.0.w),
+          child: InkWell(
+            onTap: () {},
+            child: Container(
+              margin: EdgeInsets.symmetric(vertical: 10.0.h),
+              child: Row(
+                children: [
+                  Icon(Iconsax.people, color: kTextWhite),
+                  widthSizedBox(10),
+                  Text("Devoida Workspace", style: Styles.subTitleStyle),
+                  Spacer(),
+                  Text(
+                    "Boards",
+                    style: Styles.subTitleStyle.copyWith(
+                      color: kPrimaryBlue,
+                    ),
+                  ),
+                  widthSizedBox(10),
+                  Icon(
+                    Icons.arrow_forward_ios,
+                    color: kPrimaryBlue,
+                    size: 16.sp,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        heightSizedBox(4),
+        Container(
+          height: 60.h,
+          width: double.infinity,
+          decoration: BoxDecoration(
+              border: Border.symmetric(horizontal: BorderSide(color: Colors.white.withOpacity(0.3))),
+              color: Colors.white.withOpacity(0.1)
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 12.0.w),
+            child: Row(
+              children: [
+                Container(
+                  height: 40.h,
+                  width: 50.w,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(6.r),
+                    color: Colors.pink,
+                  ),
+                ),
+                widthSizedBox(10),
+                Text("Pet-Yard", style: Styles.subTitleStyle,)
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
